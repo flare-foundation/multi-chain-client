9ae7884bfe2f7c7a0e7ee8cd390daa676161e611
AvbrehtLuka <luka@flare.network>
2022-05-17 10:35:54 +0200

    test update

--- test/BTC/template.test.ts
+++ test/BTC/template.test.ts
@@ -13,11 +13,15 @@ describe("Chain tips test ", function () {
       MccClient = new MCC.BTC(BtcMccConnection);
    });
 
-   it("Should get only latest tips ", async function () {
-      const trans = await MccClient.getTransaction('01796c39e26365b3f9d787b803760471295452b1aaf25f7cfc1ab630630b29d5');
+   it("Should temp test ", async function () {
+      const trans = await MccClient.getTransaction('a8e7ec78e5ee9589a93b937ac2f2c34a46b94571faeb8a52e8ca85c9c7b09d02');
       console.log(trans);
+
+      console.log("Augment");
+      
+
       if(trans){
-        const newTrans = await trans.paymentSummary(MccClient,0,0,true)
+        const newTrans = await trans.paymentSummary(MccClient,1,1,true)
         console.log(newTrans);
       }
       
