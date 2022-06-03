efc5f044dcf857daaa364315bbdc53bf19e84976
AvbrehtLuka <luka@flare.network>
2022-06-03 15:25:52 +0200

    added status tests

---
+++ test/DOGE/status.test.ts
@@ -0,0 +1,44 @@
+import { MCC, UtxoMccCreate, UtxoNodeStatus, XrpNodeStatus } from "../../src";
+
+const DogeMccConnection = {
+  url: process.env.DOGE_URL || '',
+  username: process.env.DOGE_USERNAME || '',
+  password: process.env.DOGE_PASSWORD || '',
+} as UtxoMccCreate;
+
+describe("Block BTC base test ", function () {
+   let MccClient: MCC.DOGE;
+   let status: UtxoNodeStatus;
+
+   before(async function () {
+      MccClient = new MCC.DOGE(DogeMccConnection);
+      const tstatus = await MccClient.getNodeStatus();
+      if(tstatus){
+         status = tstatus
+      }
+   });
+
+   it("Should get status version ", async function () {
+      console.log(status.version);
+   });
+
+   it("Should get status state ", async function () {
+      console.log(status.state);
+   });
+
+   it("Should get bottom Block ", async function () {
+      console.log(status.bottomBlock);
+   });
+
+   it("Should get status isHealthy ", async function () {
+      console.log(status.isHealthy);
+   });
+
+   it("Should get status isSynced ", async function () {
+      console.log(status.isSynced);
+   });
+
+   it("Should get full data ", async function () {
+      console.log(status.data);
+   });
+});
---
+++ test/LTC/status.test.ts
@@ -0,0 +1,44 @@
+import { MCC, UtxoMccCreate, UtxoNodeStatus, XrpNodeStatus } from "../../src";
+
+const LtcMccConnection = {
+  url: process.env.LTC_URL || '',
+  username: process.env.LTC_USERNAME || '',
+  password: process.env.LTC_PASSWORD || '',
+} as UtxoMccCreate;
+
+describe("Block BTC base test ", function () {
+   let MccClient: MCC.LTC;
+   let status: UtxoNodeStatus;
+
+   before(async function () {
+      MccClient = new MCC.LTC(LtcMccConnection);
+      const tstatus = await MccClient.getNodeStatus();
+      if(tstatus){
+         status = tstatus
+      }
+   });
+
+   it("Should get status version ", async function () {
+      console.log(status.version);
+   });
+
+   it("Should get status state ", async function () {
+      console.log(status.state);
+   });
+
+   it("Should get bottom Block ", async function () {
+      console.log(status.bottomBlock);
+   });
+
+   it("Should get status isHealthy ", async function () {
+      console.log(status.isHealthy);
+   });
+
+   it("Should get status isSynced ", async function () {
+      console.log(status.isSynced);
+   });
+
+   it("Should get full data ", async function () {
+      console.log(status.data);
+   });
+});
