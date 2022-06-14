b521b5a3cf1852aa178e04f78ffdcdff2604901d
AvbrehtLuka <luka@flare.network>
2022-06-14 13:31:48 +0200

    utxo code formating and order of methods

--- src/chain-clients/UtxoCore.ts
+++ src/chain-clients/UtxoCore.ts
@@ -71,181 +71,48 @@ export class UtxoCore implements ReadRpcInterface {
       return true;
    }
 
-   ///////////////////////////////////////////////////////////////////////////////////////
-   // Read part of RPC Client ////////////////////////////////////////////////////////////
-   ///////////////////////////////////////////////////////////////////////////////////////
-
    /**
-    * Get network info
-    * @returns network info details
+    * Return node status object for Utxo nodes
     */
-   async getNetworkInfo(retry = 0): Promise<any> {
+   async getNodeStatus(): Promise<UtxoNodeStatus | null> {
       try {
-         let res = await this.client.post("", {
+         let res = await this.client.post(``, {
             jsonrpc: "1.0",
             id: "rpc",
             method: "getnetworkinfo",
             params: [],
          });
-         // Error codes https://github.com/bitcoin/bitcoin/blob/master/src/rpc/protocol.h
-         if (utxo_check_expect_empty(res.data)) {
-            return null;
-         }
          utxo_ensure_data(res.data);
-         return res.data;
-      } catch (error) {
-         if (retry === 0) {
-            this.loggingObject.exceptionCallback(error, `UtxCore::getNetworkInfo() (failed after max retries)`);
-            return null;
-         }
-         await sleepMs(100 + getSimpleRandom(500));
-         this.loggingObject.exceptionCallback(error, `UtxCore::getNetworkInfo() (retry ${retry})`);
 
-         return await this.getNetworkInfo(retry - 1);
-      }
-   }
-
-   /**
-    * Get transaction details
-    * @param txId Transaction id
-    * @param options provide verbose:boolean, set true if you want more info such as block hash...
-    * @returns transaction details
-    */
-   async getTransaction(txId: string, options?: getTransactionOptions, retry = 3): Promise<UtxoTransaction | null> {
-      if (PREFIXED_STD_TXID_REGEX.test(txId)) {
-         txId = unPrefix0x(txId);
-      }
-      // TODO trow if txid does not match expected input
-      try {
-         let verbose = true; // by default getting transaction is in verbose mode
-         let unTxId = unPrefix0x(txId);
-         let res = await this.client.post("", {
+         let bres = await this.client.post(``, {
             jsonrpc: "1.0",
             id: "rpc",
-            method: "getrawtransaction",
-            params: [unTxId, verbose],
-         });
-         // Error codes https://github.com/bitcoin/bitcoin/blob/master/src/rpc/protocol.h
-         if (utxo_check_expect_empty(res.data)) {
-            return null;
-         }
-         utxo_ensure_data(res.data);
-         return new this.transactionConstructor(res.data.result);
-      } catch (error) {
-         if (retry === 0) {
-            this.loggingObject.exceptionCallback(error, `UtxCore::getTransaction(${txId}) (failed after max retries)`);
-            return null;
-         }
-         await sleepMs(100 + getSimpleRandom(500));
-         this.loggingObject.exceptionCallback(error, `UtxCore::getTransaction(${txId}) (retry ${retry})`);
-
-         return await this.getTransaction(txId, options, retry - 1);
-      }
-   }
-
-   /**
-    * Get Block height (number of blocks) from connected chain
-    * @returns block height (block count)
-    */
-   async getBlockHeight(retry = 3): Promise<number> {
-      try {
-         let res = await this.client.post("", {
-            jsonrpc: "1.0",
-            id: "rpc",
-            method: "getblockcount",
+            method: "getblockchaininfo",
             params: [],
          });
-         utxo_ensure_data(res.data);
-         return res.data.result;
-      } catch (error) {
-         if (retry === 0) {
-            this.loggingObject.exceptionCallback(error, `UtxCore::getTransaction() (failed after max retries)`);
-            return 0;
-         }
+         utxo_ensure_data(bres.data);
 
-         await sleepMs(100 + getSimpleRandom(500));
-
-         this.loggingObject.exceptionCallback(error, `UtxCore::getTransaction() (retry ${retry})`);
-
-         return await this.getBlockHeight(retry - 1);
-      }
-   }
-
-   // async RTRgetBlockHeight(): Promise<number> {
-   //    const baseFunc = async () => {
-   //       let res = await this.client.post("", {
-   //          jsonrpc: "1.0",
-   //          id: "rpc",
-   //          method: "getblockcount",
-   //          params: [],
-   //       });
-   //       utxo_ensure_data(res.data);
-   //       return res.data.result;
-   //    };
-   //    // return await retryTimes(baseFunc, 3, this.loggingObject.exceptionCallback);
-   //    return await retry("RTRgetBlockHeight", baseFunc)
-   // }
-
-   /**
-    * Get header information about the block
-    * @param blockHash
-    * @returns
-    */
-   async getBlockHeader(blockHashOrHeight: string | number, retry = 3): Promise<IUtxoGetBlockHeaderRes | null> {
-      try {
-         let blockHash: string | null = null;
-         if (typeof blockHashOrHeight === "string") {
-            blockHash = blockHashOrHeight as string;
-            if (PREFIXED_STD_BLOCK_HASH_REGEX.test(blockHash)) {
-               blockHash = unPrefix0x(blockHash);
-            }
-            // TODO match with some regex
-         } else if (typeof blockHashOrHeight === "number") {
-            blockHash = await this.getBlockHashFromHeight(blockHashOrHeight as number);
-         }
-         let res = await this.client.post("", {
-            jsonrpc: "1.0",
-            id: "rpc",
-            method: "getblockheader",
-            params: [blockHash],
-         });
-         if (utxo_check_expect_empty(res.data)) {
-            return null;
-         }
-         utxo_ensure_data(res.data);
-         return res.data.result;
-      } catch (error) {
-         if (retry === 0) {
-            this.loggingObject.exceptionCallback(error, `UtxCore::getBlockHeader(${blockHashOrHeight}) (failed after max retries)`);
-            return null;
-         }
-
-         await sleepMs(100 + getSimpleRandom(500));
-
-         this.loggingObject.exceptionCallback(error, `UtxCore::getBlockHeader(${blockHashOrHeight}) (retry ${retry})`);
-
-         return await this.getBlockHeader(blockHashOrHeight, retry - 1);
-      }
-   }
-
-   /**
-    * Gets the block from main mining tip with provided height
-    * @param blockNumber Block height
-    * @returns Block hash
-    */
-   async getBlockHashFromHeight(blockNumber: number): Promise<string | null> {
-      let res = await this.client.post("", {
-         jsonrpc: "1.0",
-         id: "rpc",
-         method: "getblockhash",
-         params: [blockNumber],
-      });
-      if (utxo_check_expect_block_out_of_range(res.data)) {
+         return new UtxoNodeStatus({ ...bres.data.result, ...res.data.result } as IUtxoNodeStatus);
+      } catch (e) {
          return null;
       }
-      utxo_ensure_data(res.data);
-      return res.data.result as string;
    }
+
+   /**
+    * On Utxo chains nodes always need full history
+    * @returns 0
+    */
+   async getBottomBlockHeight(): Promise<number | null> {
+      return 0;
+   }
+
+   ///////////////////////////////////////////////////////////////////////////////////////
+   // Read part of RPC Client ////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////
+
+   ///////////////////////////////////////////////////////////////////////////////////////
+   // Block methods //////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////
 
    /**
     * Returns the block information
@@ -292,25 +159,120 @@ export class UtxoCore implements ReadRpcInterface {
       }
    }
 
-   async getBlockTipsHepler(height_gte: number, mainBranchProcess?: number) {
-      // : Promise<BlockTip[]>{
-      const tips = await this.getTopBlocks({ height_gte: height_gte, all_blocks: true });
-      let mainBranchHashes: LiteBlock[] = [];
-      const activeTip = tips.filter((a) => a.status === "active")[0];
-      const ActiveTip = new LiteBlock({ hash: activeTip.hash, number: activeTip.height });
-      if (mainBranchProcess !== undefined) {
-         mainBranchHashes = await recursive_block_tip(this, ActiveTip, mainBranchProcess);
-      }
-      const allTips = tips.map((UtxoTip: IUtxoChainTip) => {
-         const tempTips = [];
-         if (UtxoTip.all_block_hashes) {
-            for (let hashIndex = 0; hashIndex < UtxoTip.all_block_hashes.length; hashIndex++) {
-               tempTips.push(new LiteBlock({ hash: UtxoTip.all_block_hashes[hashIndex], number: UtxoTip.height - hashIndex }));
-            }
+   /**
+    * Get Block height (number of blocks) from connected chain
+    * @returns block height (block count)
+    */
+   async getBlockHeight(retry = 3): Promise<number> {
+      try {
+         let res = await this.client.post("", {
+            jsonrpc: "1.0",
+            id: "rpc",
+            method: "getblockcount",
+            params: [],
+         });
+         utxo_ensure_data(res.data);
+         return res.data.result;
+      } catch (error) {
+         if (retry === 0) {
+            this.loggingObject.exceptionCallback(error, `UtxCore::getTransaction() (failed after max retries)`);
+            return 0;
          }
-         return tempTips;
-      });
-      return allTips.reduce((acc: LiteBlock[], nev: LiteBlock[]) => acc.concat(nev), []).concat(mainBranchHashes);
+
+         await sleepMs(100 + getSimpleRandom(500));
+
+         this.loggingObject.exceptionCallback(error, `UtxCore::getTransaction() (retry ${retry})`);
+
+         return await this.getBlockHeight(retry - 1);
+      }
+   }
+
+   ///////////////////////////////////////////////////////////////////////////////////////
+   // Transaction methods ////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////
+
+   /**
+    * Get transaction details
+    * @param txId Transaction id
+    * @param options provide verbose:boolean, set true if you want more info such as block hash...
+    * @returns transaction details
+    */
+   async getTransaction(txId: string, options?: getTransactionOptions, retry = 3): Promise<UtxoTransaction | null> {
+      if (PREFIXED_STD_TXID_REGEX.test(txId)) {
+         txId = unPrefix0x(txId);
+      }
+      // TODO trow if txid does not match expected input
+      try {
+         let verbose = true; // by default getting transaction is in verbose mode
+         let unTxId = unPrefix0x(txId);
+         let res = await this.client.post("", {
+            jsonrpc: "1.0",
+            id: "rpc",
+            method: "getrawtransaction",
+            params: [unTxId, verbose],
+         });
+         // Error codes https://github.com/bitcoin/bitcoin/blob/master/src/rpc/protocol.h
+         if (utxo_check_expect_empty(res.data)) {
+            return null;
+         }
+         utxo_ensure_data(res.data);
+         return new this.transactionConstructor(res.data.result);
+      } catch (error) {
+         if (retry === 0) {
+            this.loggingObject.exceptionCallback(error, `UtxCore::getTransaction(${txId}) (failed after max retries)`);
+            return null;
+         }
+         await sleepMs(100 + getSimpleRandom(500));
+         this.loggingObject.exceptionCallback(error, `UtxCore::getTransaction(${txId}) (retry ${retry})`);
+
+         return await this.getTransaction(txId, options, retry - 1);
+      }
+   }
+
+   ///////////////////////////////////////////////////////////////////////////////////////
+   // Client specific methods ////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////
+
+   /**
+    * Get header information about the block
+    * @param blockHash
+    * @returns
+    */
+   async getBlockHeader(blockHashOrHeight: string | number, retry = 3): Promise<IUtxoGetBlockHeaderRes | null> {
+      try {
+         let blockHash: string | null = null;
+         if (typeof blockHashOrHeight === "string") {
+            blockHash = blockHashOrHeight as string;
+            if (PREFIXED_STD_BLOCK_HASH_REGEX.test(blockHash)) {
+               blockHash = unPrefix0x(blockHash);
+            }
+            // TODO match with some regex
+         } else if (typeof blockHashOrHeight === "number") {
+            blockHash = await this.getBlockHashFromHeight(blockHashOrHeight as number);
+         }
+         let res = await this.client.post("", {
+            jsonrpc: "1.0",
+            id: "rpc",
+            method: "getblockheader",
+            params: [blockHash],
+         });
+         if (utxo_check_expect_empty(res.data)) {
+            return null;
+         }
+         utxo_ensure_data(res.data);
+         return res.data.result;
+      } catch (error) {
+         if (retry === 0) {
+            this.loggingObject.exceptionCallback(error, `UtxCore::getBlockHeader(${blockHashOrHeight}) (failed after max retries)`);
+            return null;
+         }
+
+         await sleepMs(100 + getSimpleRandom(500));
+
+         this.loggingObject.exceptionCallback(error, `UtxCore::getBlockHeader(${blockHashOrHeight}) (retry ${retry})`);
+
+         return await this.getBlockHeader(blockHashOrHeight, retry - 1);
+      }
    }
 
    /**
@@ -318,13 +280,18 @@ export class UtxoCore implements ReadRpcInterface {
     * @param height_gte
     * @returns
     */
-   async getBlockTips(height_gte: number) {
-      return this.getBlockTipsHepler(height_gte);
+   async getBlockTips(height_gte: number): Promise<LiteBlock[]> {
+      return this.getBlockTipsHelper(height_gte);
    }
 
-   async getTopLiteBlocks(branch_len: number) {
+   /**
+    * Get block tips and full block chain for all tips (all of heir branches) for the last x blocks (defined bt branch_len parameter)
+    * @param branch_len the branch length indicating how long can branches be
+    * @returns Array of LiteBlocks
+    */
+   async getTopLiteBlocks(branch_len: number): Promise<LiteBlock[]> {
       const height = await this.getBlockHeight();
-      return this.getBlockTipsHepler(height - branch_len, branch_len);
+      return this.getBlockTipsHelper(height - branch_len, branch_len);
    }
 
    /**
@@ -356,44 +323,85 @@ export class UtxoCore implements ReadRpcInterface {
       return response;
    }
 
+   ///////////////////////////////////////////////////////////////////////////////////////
+   // Client helper (private) methods ////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////
+
    /**
-    * Node status query
+    * Get network info
+    * @returns network info details
     */
-   async getNodeStatus(): Promise<UtxoNodeStatus | null> {
+   private async getNetworkInfo(retry = 0): Promise<any> {
       try {
-         let res = await this.client.post(``, {
+         let res = await this.client.post("", {
             jsonrpc: "1.0",
             id: "rpc",
             method: "getnetworkinfo",
             params: [],
          });
+         // Error codes https://github.com/bitcoin/bitcoin/blob/master/src/rpc/protocol.h
+         if (utxo_check_expect_empty(res.data)) {
+            return null;
+         }
          utxo_ensure_data(res.data);
+         return res.data;
+      } catch (error) {
+         if (retry === 0) {
+            this.loggingObject.exceptionCallback(error, `UtxCore::getNetworkInfo() (failed after max retries)`);
+            return null;
+         }
+         await sleepMs(100 + getSimpleRandom(500));
+         this.loggingObject.exceptionCallback(error, `UtxCore::getNetworkInfo() (retry ${retry})`);
 
-         let bres = await this.client.post(``, {
-            jsonrpc: "1.0",
-            id: "rpc",
-            method: "getblockchaininfo",
-            params: [],
-         });
-         utxo_ensure_data(bres.data);
-
-         return new UtxoNodeStatus({ ...bres.data.result, ...res.data.result } as IUtxoNodeStatus);
-      } catch (e) {
-         return null;
+         return await this.getNetworkInfo(retry - 1);
       }
    }
 
    /**
-    * On Utxo chains nodes always need full history
-    * @returns 0
+    * Gets the block from main mining tip with provided height
+    * @param blockNumber Block height
+    * @returns Block hash
     */
-   async getBottomBlockHeight(): Promise<number | null> {
-      return 0;
+   private async getBlockHashFromHeight(blockNumber: number): Promise<string | null> {
+      let res = await this.client.post("", {
+         jsonrpc: "1.0",
+         id: "rpc",
+         method: "getblockhash",
+         params: [blockNumber],
+      });
+      if (utxo_check_expect_block_out_of_range(res.data)) {
+         return null;
+      }
+      utxo_ensure_data(res.data);
+      return res.data.result as string;
+   }
+
+   private async getBlockTipsHelper(height_gte: number, mainBranchProcess?: number) {
+      // : Promise<BlockTip[]>{
+      const tips = await this.getTopBlocks({ height_gte: height_gte, all_blocks: true });
+      let mainBranchHashes: LiteBlock[] = [];
+      const activeTip = tips.filter((a) => a.status === "active")[0];
+      const ActiveTip = new LiteBlock({ hash: activeTip.hash, number: activeTip.height });
+      if (mainBranchProcess !== undefined) {
+         mainBranchHashes = await recursive_block_tip(this, ActiveTip, mainBranchProcess);
+      }
+      const allTips = tips.map((UtxoTip: IUtxoChainTip) => {
+         const tempTips = [];
+         if (UtxoTip.all_block_hashes) {
+            for (let hashIndex = 0; hashIndex < UtxoTip.all_block_hashes.length; hashIndex++) {
+               tempTips.push(new LiteBlock({ hash: UtxoTip.all_block_hashes[hashIndex], number: UtxoTip.height - hashIndex }));
+            }
+         }
+         return tempTips;
+      });
+      return allTips.reduce((acc: LiteBlock[], nev: LiteBlock[]) => acc.concat(nev), []).concat(mainBranchHashes);
    }
 
    ///////////////////////////////////////////////////////////////////////////////////////
    // Write part of RPC Client ///////////////////////////////////////////////////////////
    ///////////////////////////////////////////////////////////////////////////////////////
+
+   // Not yet tested or used 
 
    /**
     * Creates a new wallet on node's database
