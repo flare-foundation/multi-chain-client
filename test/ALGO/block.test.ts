d1f72c80fc13ae27b41340c2af00dec398d52f6a
AvbrehtLuka <luka@flare.network>
2022-06-17 00:25:37 +0200

    algo block tests

--- test/ALGO/block.test.ts
+++ test/ALGO/block.test.ts
@@ -1,56 +1,63 @@
+import { expect } from "chai";
 import { AlgoBlock, MCC } from "../../src";
 
 const algoCreateConfig = {
    algod: {
       url: process.env.ALGO_ALGOD_URL || "",
       token: process.env.ALGO_ALGOD_TOKEN || "",
-   },
-   indexer: {
-      url: process.env.ALGO_INDEXER_URL || "",
-      token: process.env.ALGO_INDEXER_TOKEN || "",
-   },
+   }
 };
 
 describe(`Algo block processing`, async () => {
    describe("Classic block test ", function () {
       let MccClient: MCC.ALGO;
       let block: AlgoBlock;
+      const blockNumber = 21_659_776
 
       before(async function () {
          MccClient = new MCC.ALGO(algoCreateConfig);
-         block = await MccClient.getBlock(21_659_776);
+         block = await MccClient.getBlock(blockNumber);
       });
 
       it("Should get block number ", async function () {
-         console.log(block.number);
+         expect(block.number).to.eq(blockNumber)
       });
 
       it("Should get block hash ", async function () {
-         console.log(block.blockHash);
+         expect(block.blockHash).to.eq("rWPGk6t3twBulPE4lt7Yt9IHpgeOsqU1IlGAGJ5B74g=")
+      });
+
+      it("Should get block hash base32 ", async function () {
+         expect(block.blockHashBase32).to.eq("VVR4NE5LO63QA3UU6E4JNXWYW7JAPJQHR2ZKKNJCKGABRHSB56EA")
+      });
+
+      it("Should get block hash base64 ", async function () {
+         expect(block.blockHashBase64).to.eq("rWPGk6t3twBulPE4lt7Yt9IHpgeOsqU1IlGAGJ5B74g=")
       });
 
       it("Should get block standard hash ", async function () {
-         console.log(block.stdBlockHash);
+         expect(block.stdBlockHash).to.eq("ad63c693ab77b7006e94f13896ded8b7d207a6078eb2a535225180189e41ef88")
       });
 
       it("Should get block timestamp ", async function () {
-         console.log(block.unixTimestamp);
+         expect(block.unixTimestamp).to.eq(1655413908)
       });
 
       it("Should get transaction ids ", async function () {
-         console.log(block.transactionIds);
+         expect(block.transactionIds.length).to.eq(69)
+         // Todo at least check some txids
       });
 
       it("Should get transaction standard ids ", async function () {
-         console.log(block.stdTransactionIds);
+         expect(block.stdTransactionIds.length).to.eq(69)
       });
 
       it("Should get transaction count ", async function () {
-         console.log(block.transactionCount);
+         expect(block.transactionCount).to.eq(69)
       });
 
       it("Should get transaction objects (ALGO SPECIFIC) ", async function () {
-         console.log(block.transactions.map((el) => el.txid));
+         expect(block.transactions.length).to.eq(69)
       });
    });
 });
