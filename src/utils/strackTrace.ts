4fc0230682e9e4c7d3eea840ec0f2b4dca439bf4
David Pangerl <david@altumvi.com>
2022-06-20 11:03:42 +0200

    trace updates

---
+++ src/utils/strackTrace.ts
@@ -0,0 +1,65 @@
+import { TraceCall } from "./trace";
+
+export class StackTraceCall {
+    method: string = "";
+    source: string = "";
+    line: number = -1;
+    column: number=0;
+
+    constructor(stackTrace: string) {
+        if (stackTrace.indexOf("(") > 0) {
+            const traceLine = /\s*at ([^(]*)\(([^\:]*)\:(\d+)\:(\d+)\)/.exec(stackTrace);
+            if (traceLine && traceLine.length === 5) {
+                this.method = traceLine[1].trim();
+                this.source = traceLine[2].trim();
+                this.line = parseInt(traceLine[3]);
+                this.column = parseInt(traceLine[4]);
+            }
+        }
+        else {
+            const traceLine = /\s*at ([^\:]*)\:(\d+)\:(\d+)/.exec(stackTrace);
+            if (traceLine && traceLine.length === 4) {
+                this.method = "";
+                this.source = traceLine[1].trim();
+                this.line = parseInt(traceLine[2]);
+                this.column = parseInt(traceLine[3]);
+            }
+        }
+    }
+
+    get isValid() {
+        return this.line >= 0;
+    }
+}
+
+export class StackTrace {
+    stackTrace: StackTraceCall[] = [];
+
+    constructor() {
+        const stack = Error().stack!;
+
+        const trace = stack.split("\n");
+
+        this.stackTrace = [];
+
+        for (let traceLine of trace) {
+
+            const trace = new StackTraceCall(traceLine);
+            if (trace.isValid) {
+                this.stackTrace.push(trace);
+            }
+        }
+    }
+
+    find(method: string, functionOffset=0): StackTraceCall | undefined {
+        for(let i=0; i<this.stackTrace.length; i++) {
+            if( this.stackTrace[i].method===method ) {
+                if( i + functionOffset >=0 && i + functionOffset < this.stackTrace.length ) {
+                    return this.stackTrace[i+functionOffset];
+                }
+            }
+        }
+        return undefined;
+    }
+
+}
