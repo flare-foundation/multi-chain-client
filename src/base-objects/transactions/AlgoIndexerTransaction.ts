47362d19cf7cb024594ff80f8c37fc403a010073
grepod <gregor.podlogar@aflabs.si>
2022-09-15 16:43:42 +0200

    Getting info about asset from indexer.

--- src/base-objects/transactions/AlgoIndexerTransaction.ts
+++ src/base-objects/transactions/AlgoIndexerTransaction.ts
@@ -1,6 +1,7 @@
 import BN from "bn.js";
+import { MCC } from "../..";
 import { MccClient, TransactionSuccessStatus } from "../../types";
-import { AlgoTransactionTypeOptions, IAlgoGetTransactionRes } from "../../types/algoTypes";
+import { AlgoTransactionTypeOptions, IAlgoGetTransactionRes, IAlgoIndexerAdditionalData } from "../../types/algoTypes";
 import { base64ToHex, txIdToHexNo0x } from "../../utils/algoUtils";
 import { ALGO_MDU, ALGO_NATIVE_TOKEN_NAME } from "../../utils/constants";
 import { Managed } from "../../utils/managed";
@@ -9,22 +10,18 @@ import { AddressAmount, PaymentSummary, TransactionBase } from "../TransactionBa
 const web3 = require("web3");
 
 @Managed()
-export class AlgoIndexerTransaction extends TransactionBase<IAlgoGetTransactionRes, any> {
-   public makeFull(client: MccClient): Promise<void> {
-      throw new Error("Method not implemented.");
+export class AlgoIndexerTransaction extends TransactionBase<IAlgoGetTransactionRes, IAlgoIndexerAdditionalData> {
+   public async makeFull(client: MCC.ALGO): Promise<void> {
+      if (!this.additionalData) {
+         this.additionalData = {};
+      }
+      if (!this.additionalData.assetInfo) {
+         if (this.data.transaction.assetTransferTransaction) {
+            this.additionalData.assetInfo = await client.getIndexerAssetInfo(this.data.transaction.assetTransferTransaction.assetId);
+         }
+      }
    }
-   public get assetSourceAddresses(): (string | undefined)[] {
-      throw new Error("Method not implemented.");
-   }
-   public get assetReceivingAddresses(): (string | undefined)[] {
-      throw new Error("Method not implemented.");
-   }
-   public get assetSpentAmounts(): AddressAmount[] {
-      throw new Error("Method not implemented.");
-   }
-   public get assetReceivedAmounts(): AddressAmount[] {
-      throw new Error("Method not implemented.");
-   }
+
    public get txid(): string {
       return this.hash;
    }
@@ -41,7 +38,7 @@ export class AlgoIndexerTransaction extends TransactionBase<IAlgoGetTransactionR
    }
 
    public get reference(): string[] {
-      if(this.data.transaction.note) {
+      if (this.data.transaction.note) {
          return [base64ToHex(this.data.transaction.note)];
       }
       return [];
@@ -72,13 +69,40 @@ export class AlgoIndexerTransaction extends TransactionBase<IAlgoGetTransactionR
       return [this.data.transaction.sender];
    }
 
+   public get assetSourceAddresses(): (string | undefined)[] {
+      if (this.type === "axfer" || this.type === "axfer_close") {
+         // for token transfers send by clawback transaction (https://developer.algorand.org/docs/get-details/transactions/transactions/#asset-clawback-transaction)
+         // and asset transfer transactions (https://developer.algorand.org/docs/get-details/transactions/transactions/#asset-transfer-transaction)
+         if (this.data.transaction.assetTransferTransaction.sender) {
+            // for asset clawback tx it somehow gets addess w/out checksum
+            return [this.data.transaction.assetTransferTransaction.sender];
+         }
+         // Other kinds of asset transactions (axfer and axfer_close ) use snd field as sender
+         return [this.data.transaction.sender];
+      }
+      // Other kinds of transactions have nothing to do with assets
+      return [];
+   }
+
    public get receivingAddresses(): string[] {
       if (this.type === "pay" && this.data.transaction.paymentTransaction) {
          return [this.data.transaction.paymentTransaction.receiver];
-      } else if (this.type === "axfer" && this.data.transaction.assetTransferTransaction) {
-         return [this.data.transaction.assetTransferTransaction.receiver];
       }
       // TODO check other transaction types
+      return [];
+   }
+
+   public get assetReceivingAddresses(): (string | undefined)[] {
+      const recAddresses = [];
+      if (this.type === "axfer" || this.type === "axfer_close") {
+         if (this.data.transaction.assetTransferTransaction.receiver) {
+            recAddresses.push(this.data.transaction.assetTransferTransaction.receiver);
+         }
+         if (this.data.transaction.assetTransferTransaction.closeTo) {
+            recAddresses.push(this.data.transaction.assetTransferTransaction.closeTo);
+         }
+         return recAddresses;
+      }
       return [];
    }
 
@@ -94,13 +118,6 @@ export class AlgoIndexerTransaction extends TransactionBase<IAlgoGetTransactionR
                amount: this.fee.add(toBN(this.data.transaction.paymentTransaction.amount)),
             },
          ];
-      } else if (this.data.transaction.txType === "axfer" && this.data.transaction.assetTransferTransaction) {
-         return [
-            {
-               address: this.sourceAddresses[0],
-               amount: this.fee.add(toBN(this.data.transaction.assetTransferTransaction.amount)),
-            },
-         ];
       } else {
          return [
             {
@@ -109,6 +126,10 @@ export class AlgoIndexerTransaction extends TransactionBase<IAlgoGetTransactionR
             },
          ];
       }
+   }
+
+   public get assetSpentAmounts(): AddressAmount[] {
+      throw new Error("Method not implemented.");
    }
 
    public get receivedAmounts(): AddressAmount[] {
@@ -128,6 +149,10 @@ export class AlgoIndexerTransaction extends TransactionBase<IAlgoGetTransactionR
          ];
       }
       return [];
+   }
+
+   public get assetReceivedAmounts(): AddressAmount[] {
+      throw new Error("Method not implemented.");
    }
 
    public get type(): AlgoTransactionTypeOptions {
