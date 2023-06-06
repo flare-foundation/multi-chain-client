1c01395ead10d3f0fa5fd6e4eacc1a6345d51300
AvbrehtLuka <luka@flare.network>
2023-06-06 10:51:11 +0200

    beta doge

--- src/base-objects/fullBlocks/DogeFullBlock.ts
+++ src/base-objects/fullBlocks/DogeFullBlock.ts
@@ -1,10 +1,20 @@
+import { IUtxoGetBlockRes } from "../../types";
 import { Managed } from "../../utils/managed";
-import { FullBlockBase, UtxoBlock } from "../BlockBase";
+import { FullBlockBase, UtxoBlock, UtxoFullBlock } from "../BlockBase";
 import { DogeTransaction } from "../TransactionBase";
 
+// @Managed()
+// export class DogeFullBlock extends UtxoBlock implements FullBlockBase<DogeTransaction> {
+//    public get transactions(): DogeTransaction[] {
+//       throw new Error("Method not implemented.");
+//    }
+// }
+
 @Managed()
-export class DogeFullBlock extends UtxoBlock implements FullBlockBase<DogeTransaction> {
-   public get transactions(): DogeTransaction[] {
-      throw new Error("Method not implemented.");
+export class DogeFullBlock extends UtxoFullBlock<DogeTransaction> {
+   constructor(data: IUtxoGetBlockRes) {
+      super(data);
+      // Must be implemented in specific chain class
+      this.transactionConstructor = DogeTransaction;
    }
 }
