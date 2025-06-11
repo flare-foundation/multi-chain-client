7f99e7926578dc3d9d689aeb60da82858be02c6c
Luka Avbreht <luka@flare.network>
2025-06-11 23:25:32 +0200

    fix: updated balance decreasing definition

--- test/XRP/balanceDecreasing.test.ts
+++ test/XRP/balanceDecreasing.test.ts
@@ -20,7 +20,7 @@ describe(`summary tests, ${getTestFile(__filename)}`, function () {
 
     it("Should be able to extract balance decreasing", async function () {
         const tx_id = "27D592539E1FB00E8E4C4B6022729CB5559DE94AA2285AE510664A7E0891B3DB";
-        const transaction = await retry("nekej", () => MccClient.getTransaction(tx_id));
+        const transaction = await retry("XRP.getTransaction", () => MccClient.getTransaction(tx_id));
         const dec = transaction.balanceDecreasingSummary(standardAddressHash("r4s3spDTkS5xZoQujwEzbjgep3NUPTiHyq"));
         expect(dec.status).to.eq("success");
         if (dec.response) {
@@ -38,7 +38,7 @@ describe(`summary tests, ${getTestFile(__filename)}`, function () {
 
     it("Should be able to compute nonexistence summary", async function () {
         const tx_id = "27D592539E1FB00E8E4C4B6022729CB5559DE94AA2285AE510664A7E0891B3DB";
-        const transaction = await retry("nekej", () => MccClient.getTransaction(tx_id));
+        const transaction = await retry("XRP.getTransaction", () => MccClient.getTransaction(tx_id));
         const summary = transaction.paymentNonexistenceSummary();
         expect(summary.status).to.eq(PaymentNonexistenceSummaryStatus.Success);
         if (summary.response) {
@@ -49,4 +49,23 @@ describe(`summary tests, ${getTestFile(__filename)}`, function () {
             expect(decRes.receivedAmount).to.eq(3008927646n);
         }
     });
+
+    it("Should be able to extract balance decreasing", async function () {
+        const tx_id = "C034A5716E451E33BE1D5827B7B995976F2E846BD1810EDD6EFB6BA4C1B32AB3";
+        const transaction = await retry("XRP.getTransaction", () => MccClient.getTransaction(tx_id));
+        const dec = transaction.balanceDecreasingSummary(standardAddressHash("rKi8uT8GQsyKvNo4ie4MVGsqsLUjG3noEy"));
+        expect(dec.status).to.eq("success");
+        if (dec.response) {
+            const decRes = dec.response;
+            expect(decRes.blockTimestamp).to.eq(1749671961);
+            expect(decRes.transactionHash).to.eq(tx_id);
+            expect(decRes.sourceAddressHash).to.eq(standardAddressHash("rKi8uT8GQsyKvNo4ie4MVGsqsLUjG3noEy"));
+            expect(decRes.sourceAddress).to.eq("rKi8uT8GQsyKvNo4ie4MVGsqsLUjG3noEy");
+            expect(decRes.spentAmount.toString()).to.eq("12");
+            expect(decRes.paymentReference).to.eq(ZERO_BYTES_32);
+            expect(decRes.sourceAddressIndicator).to.eq(standardAddressHash("rKi8uT8GQsyKvNo4ie4MVGsqsLUjG3noEy"));
+            expect(decRes.isFull).to.eq(true);
+        }
+    });
+
 });
